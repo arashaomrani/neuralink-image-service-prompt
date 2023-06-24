0bec0114a8e8d3ec37a64f7ac06da5fbeb58178d
arash omrani <arash.a.omrani@gmail.com>
2023-06-23 20:47:06 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y python3.8 python3-pip
+brew update
+brew install python3.8 python3-pip
 pip3 install grpcio grpcio-tools protobuf pillow numpy scipy opencv-python
